6d1d2d18f16c5bb5a3eb0d0c6b3183fe7728da08
JorgeJoseAbad <jorgejoseabad@gmail.com>
2021-04-06 22:10:24 +0200

    api key oculta

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Google Sites' });
+  res.render('index', {
+    title: 'Google Sites',
+    API_KEY : process.env.API_KEY
+   });
 });
 
 module.exports = router;
